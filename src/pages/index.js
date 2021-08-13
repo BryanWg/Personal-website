9d6e598c4d9f511bb9debdb881a79da5cc6489c0
BryanWg <wongbryan1@gmail.com>
2021-08-13 09:15:28 +0800

    hehe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import Layout from "../components/layout";
 import Terminal from "../components/terminal";
-import Astronaut from '../images/astronaut'
+import Astronaut from '../images/astronaut';
 
 const IndexPage = () => {
   return (
     <main >
       <Layout>
-        <div className="flex justify-evenly">
-          <Terminal/>
-          <Astronaut/>
+        <div className="flex flex-col">
+          <h1 className=" text-5xl text-white font-serif text-center mb-5">
+            Welcome 👋
+          </h1>
+          <div className="flex justify-evenly">
+            <Terminal />
+            <Astronaut />
+          </div>
         </div>
+
       </Layout>
     </main>
   );
